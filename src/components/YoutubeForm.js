22ba68f484328a18c21eb9c4a08539b4f8f862b7
@Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2020-10-31 14:54:19 +0530

    adding validate method

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,24 +1,72 @@
 import { useFormik } from "formik";
 
+const initialValues = {
+    name: "",
+    email: "",
+    channel: "",
+};
+
+const onSubmit = (values) => {
+    console.log("Form data", values);
+};
+
+const validate = (values) => {
+    let errors = {};
+
+    if (!values.name) {
+        errors.name = "Required";
+    }
+
+    if (!values.email) {
+        errors.email = "Required";
+    } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+        errors.email = "Invalid email format";
+    }
+
+    if (!values.channel) {
+        errors.channel = "Required";
+    }
+    return errors;
+};
+
 function YoutubeForm() {
     const formik = useFormik({
-        initialValues: {
-            name: "",
-            email: "",
-            channel: "",
-        },
+        initialValues,
+        onSubmit,
+        validate,
     });
+
     return (
         <div>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" />
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    onChange={formik.handleChange}
+                    value={formik.values.name}
+                />
 
                 <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" name="email" />
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
 
                 <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" name="channel" />
+                <input
+                    type="text"
+                    id="channel"
+                    name="channel"
+                    onChange={formik.handleChange}
+                    value={formik.values.channel}
+                />
 
                 <button type="submit">Submit</button>
             </form>
